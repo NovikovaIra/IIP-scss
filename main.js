5e6eaea92beea1cecc2740802dba0cd5705222c8
Novikova Ira <novikova.mrr@gmail.com>
2023-11-24 16:43:32 +0300

    Made all the pages

--- main.js
+++ main.js
@@ -36,10 +36,12 @@ const swiper = new Swiper('.swiper', {
         }
     }
   });
-  let menuBtn = document.querySelector('.menu-btn');
-  let menu = document.querySelector('.menu');
-  
-  menuBtn.addEventListener('click', function(){
-      menuBtn.classList.toggle('active');
-      menu.classList.toggle('active');
-  })
+
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+let body = document.querySelector('body');
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+    body.classList.toggle('noactive');
+})
